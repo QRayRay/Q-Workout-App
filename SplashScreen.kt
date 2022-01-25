3cee9d0f402ab8070e1210221ef9ded12c117fae
Raymond C Quintana <77255338+QRayRay@users.noreply.github.com>
2022-01-24 19:20:19 -0600

    Update SplashScreen.kt

--- SplashScreen.kt
+++ SplashScreen.kt
@@ -2,8 +2,8 @@ package com.example.qworkoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-// @splash image to start screen. The image has to be a gif image from slash 
-// when the activity opens. After that, please go ahead and select a gym icon to press to enter the next activity.
+
+
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
