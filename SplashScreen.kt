e3b9336d394a3324d43325f02014812d10f96bf3
Raymond C Quintana <77255338+QRayRay@users.noreply.github.com>
2022-02-02 21:42:32 -0600

    Update SplashScreen.kt

--- SplashScreen.kt
+++ SplashScreen.kt
@@ -3,6 +3,7 @@ package com.example.qworkoutapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+val btnClickMe = findViewByID<Button>()
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,3 +11,4 @@ class SplashScreen : AppCompatActivity() {
         setContentView(R.layout.activity_splash_screen)
     }
 }
+
